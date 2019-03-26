2c0eb4d97fdc19f84c0f0c395fb76f551b48e4c2
Maritza Palacios <maritza.palacios@l1to1.org>
2019-03-25 22:09:44 -0500

    initial commit

---
+++ src/components/Firebase/firebase.js
@@ -0,0 +1,37 @@
+import app from 'firebase/app';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyDhxBsQI6a7X7BCM3Wk42yu1wDUt8C3ilY",
+    authDomain: "animals-adoption.firebaseapp.com",
+    databaseURL: "https://animals-adoption.firebaseio.com",
+    projectId: "animals-adoption",
+    storageBucket: "animals-adoption.appspot.com",
+    messagingSenderId: "478854852573"
+};
+//firebase.initializeApp(config);
+
+class Firebase {
+    constructor() {
+      app.initializeApp(config);
+
+      this.auth = app.auth();
+    }
+
+    // *** Auth API ***
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+    this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+    doPasswordUpdate = password =>
+    this.auth.currentUser.updatePassword(password);
+  }
+  
+  export default Firebase;
